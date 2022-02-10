70c38803c1d1887e7e0c3210f3cd202c0333a287
Chayanit <chayanit.dev@gmail.com>
2022-02-10 22:38:19 +0700

    Add bookmark message bottom

--- components/Bookmark.tsx
+++ components/Bookmark.tsx
@@ -1,14 +1,57 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import styled from 'styled-components'
 
+interface Props {
+    isAdd: boolean
+}
+
 export default function BookmarkComponent() {
-    const [isAddBookmark, setIsAddBookmark] = useState(true)
+    const [isAddBookmark, setIsAddBookmark] = useState(false)
+    const [isButtonClicked, setIsButtonClicked] = useState(false)
+
+    useEffect(() => {
+        renderTabMessage()
+    }, [isAddBookmark])
+
+    const onClickButton = () => {
+        setIsAddBookmark(!isAddBookmark)
+        setIsButtonClicked(true)
+    }
+
+    const renderTabMessage = () => {
+        if (isButtonClicked) {
+            if (isAddBookmark) {
+                return (
+                    <TabMessage isAdd={isAddBookmark}>
+                        <BookmarkIcon
+                            src="/icons/bookmark.svg"
+                            alt="bookmark"
+                        />
+                        saved to bookmarks
+                    </TabMessage>
+                )
+            } else {
+                return (
+                    <TabMessage isAdd={isAddBookmark}>
+                        <BookmarkIcon
+                            src="/icons/bookmark-off.svg"
+                            alt="bookmark"
+                        />
+                        removed from bookmarks
+                    </TabMessage>
+                )
+            }
+        }
+    }
 
     return (
-        <Button onClick={() => setIsAddBookmark(!isAddBookmark)}>
-            <BookmarkIcon src="/icons/bookmark.svg" alt="bookmark" />
-            {isAddBookmark ? 'ADD BOOKMARK' : 'REMOVE BOOKMARK'}
-        </Button>
+        <>
+            <Button onClick={() => onClickButton()}>
+                <BookmarkIcon src="/icons/bookmark.svg" alt="bookmark" />
+                {isAddBookmark ? 'REMOVE BOOKMARK' : 'ADD BOOKMARK'}
+            </Button>
+            {renderTabMessage()}
+        </>
     )
 }
 
@@ -30,8 +73,25 @@ const Button = styled.button`
 `
 
 const BookmarkIcon = styled.img`
-    /* position: absolute; */
     height: 18px;
     width: 18px;
     margin-right: 6px;
+`
+const TabMessage = styled.div<Props>`
+    position: fixed;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: 20;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    height: 32px;
+    font-family: 'Roboto', sans-serif;
+    font-weight: 500;
+    font-size: 14px;
+    line-height: 24px;
+    color: #fff;
+    text-transform: uppercase;
+    background-color: ${(props) => (props.isAdd ? '#388E3C' : '#D32F2F')};
 `
