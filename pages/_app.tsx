3cb3bfb796f9d564d5f88a97326664829aefeea3
Chayanit <chayanit.dev@gmail.com>
2022-02-11 23:20:36 +0700

    Edit container width

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -25,38 +25,44 @@ const GlobalStyle = createGlobalStyle`
 `
 
 const theme = {
-  colors: {
-    primary: '#09357B',
-  },
-  breakpoint: {
-    sm: '575px',
-    md: '767px',
-    lg: '991px',
-    xl: '1200px'
-  },
-  layout: {
-    width: '1100px',
-  }
+    colors: {
+        primary: '#09357B',
+    },
+    breakpoint: {
+        sm: '575px',
+        md: '767px',
+        lg: '991px',
+        xl: '1200px',
+    },
+    layout: {
+        width: '1132px',
+    },
 }
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <GlobalStyle />
-      <ThemeProvider theme={theme}>
-        <Head>
-           <meta charSet="utf-8" />
-           <meta name="viewport" content="width=device-width" />
-          <meta name="apple-mobile-web-app-capable" content="yes" />
-          <title>The Peaks</title>
-          <meta name="title" content="The Peaks" />
-          <meta name="theme-color" content={theme.colors.primary} />
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap" rel="stylesheet" />
-        </Head>
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </>
-  )
+    return (
+        <>
+            <GlobalStyle />
+            <ThemeProvider theme={theme}>
+                <Head>
+                    <meta charSet="utf-8" />
+                    <meta name="viewport" content="width=device-width" />
+                    <meta name="apple-mobile-web-app-capable" content="yes" />
+                    <title>The Peaks</title>
+                    <meta name="title" content="The Peaks" />
+                    <meta name="theme-color" content={theme.colors.primary} />
+                    <link
+                        rel="preconnect"
+                        href="https://fonts.googleapis.com"
+                    />
+                    <link rel="preconnect" href="https://fonts.gstatic.com" />
+                    <link
+                        href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap"
+                        rel="stylesheet"
+                    />
+                </Head>
+                <Component {...pageProps} />
+            </ThemeProvider>
+        </>
+    )
 }
